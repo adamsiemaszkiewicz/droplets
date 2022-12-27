26dae976ac78cee83dbb4ae6cb93746dfd5479cc
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2022-12-27 22:39:24 +0100

    Save df to feather file.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
 import logging
+from datetime import datetime
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from matplotlib.image import imread
 from pandas import DataFrame, Series
+from tqdm import tqdm
 
 from src import consts
 
+tqdm.pandas()
 logging.basicConfig(level=logging.INFO)
 
 SEED = 42
 
 
 def extract_sample_description(filepath: Path) -> Series:
-    parts = filepath.parts
-    sample_info = parts[0]
+
+    sample_info = filepath.parent.name
     sample_info_parts = sample_info.split("_")
 
     name = sample_info_parts[0]
@@ -25,7 +29,7 @@ def extract_sample_description(filepath: Path) -> Series:
     return pd.Series([name, concentration])
 
 
-def display_samples(df: DataFrame, n_samples: int, random_state: int = SEED):
+def display_samples(df: DataFrame, n_samples: int, random_state: int = SEED) -> None:
 
     _df = df.sample(n=n_samples, random_state=random_state)
     filepaths = _df["filepath"]
@@ -35,7 +39,7 @@ def display_samples(df: DataFrame, n_samples: int, random_state: int = SEED):
     fig, axs = plt.subplots(n_samples, 1)
 
     for ax, fp, n, c in zip(axs, filepaths, names, concentrations):
-        image = imread(consts.DATA_DIR / fp)
+        image = imread(fp)
         ax.imshow(image)
         ax.axis("off")
         ax.set_title(f"{n}: {c}mg/l")
@@ -44,13 +48,25 @@ def display_samples(df: DataFrame, n_samples: int, random_state: int = SEED):
     plt.show()
 
 
+def calculate_sample_statistics(filepath: Path) -> Series:
+    image = imread(filepath)
+    mean = np.mean(image)
+    std = np.std(image)
+
+    return Series([mean, std])
+
+
 def main():
     absolute_filepaths = sorted(list(consts.DATA_DIR.glob("*/*.jpg")))
-    relative_filepaths = [p.relative_to(consts.DATA_DIR) for p in absolute_filepaths]
-    df = pd.DataFrame(data=relative_filepaths, columns=["filepath"])
-    df[["name", "concentration"]] = df["filepath"].apply(lambda x: extract_sample_description(x))
-
+    df = pd.DataFrame(data=absolute_filepaths, columns=["filepath"])
+    df[["name", "concentration"]] = df["filepath"].progress_apply(lambda x: extract_sample_description(filepath=x))
+    df[["mean", "std"]] = df["filepath"].progress_apply(lambda x: calculate_sample_statistics(filepath=x))
     display_samples(df=df, n_samples=4)
+
+    df["filepath"] = df["filepath"].progress_apply(lambda x: x.relative_to(consts.DATA_DIR).as_posix())
+
+    out_fp = consts.DATA_DIR / f"dataset_{datetime.now().date().isoformat()}.feather"
+    df.to_feather(path=out_fp)
 
 
 if __name__ == "__main__":
